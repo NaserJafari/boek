de1e51f97c77e4c8f48ad808ad027e56f4eb5650
Suhaayb <1197145@student.roc-nijmegen.nl>
2023-06-20 11:34:37 +0200

    Book done

--- book.js
+++ book.js
@@ -2,16 +2,13 @@ const prevBtn = document.querySelector('#prev-btn');
 const nextBtn = document.querySelector('#next-btn');
 const book = document.querySelector('#book');
 
-const paper1 = document.querySelector('#p1');
-const paper2 = document.querySelector('#p2');
-const paper3 = document.querySelector('#p3');
+const papers = document.querySelectorAll('.paper');
 
 prevBtn.addEventListener('click', goPrevPage);
 nextBtn.addEventListener('click', goNextPage);
 
-let currentLocation = 1;
-let numOfPapers = 3;
-let maxLocation = numOfPapers + 1;
+let currentLocation = 0;
+const numOfPapers = papers.length;
 
 function openBook() {
   book.style.transform = 'translateX(50%)';
@@ -31,53 +28,33 @@ function closeBook(isAtBeginning) {
 }
 
 function goNextPage() {
-  if (currentLocation < maxLocation) {
-    switch (currentLocation) {
-      case 1:
-        openBook();
-        paper1.classList.add('flipped');
-        paper1.style.zIndex = 1;
-        break;
-      case 2:
-        paper2.classList.add('flipped');
-        paper2.style.zIndex = 2;
-        break;
-      case 3:
-        paper3.classList.add('flipped');
-        paper3.style.zIndex = 3;
-        closeBook(false);
-        break;
-      default:
-        throw new Error('unkown state');
+  if (currentLocation < numOfPapers) {
+    const currentPage = papers[currentLocation];
+    currentPage.classList.add('flipped');
+    currentPage.style.zIndex = currentLocation + 1;
+
+    if (currentLocation === numOfPapers - 1) {
+      closeBook(false);
     }
+
     currentLocation++;
   }
 }
 
 function goPrevPage() {
-  if (currentLocation > 1) {
-    switch (currentLocation) {
-      case 2:
-        closeBook(true);
-        paper1.classList.remove('flipped');
-        paper1.style.zIndex = 3;
-        break;
-      case 3:
-        paper2.classList.remove('flipped');
-        paper2.style.zIndex = 2;
-        break;
-      case 4:
-        openBook();
-        paper3.classList.remove('flipped');
-        paper3.style.zIndex = 1;
-        break;
-      default:
-        throw new Error('unkown state');
-    }
-
+  if (currentLocation > 0) {
     currentLocation--;
+
+    const currentPage = papers[currentLocation];
+    currentPage.classList.remove('flipped');
+    currentPage.style.zIndex = numOfPapers - currentLocation;
+
+    if (currentLocation === 0) {
+      closeBook(true);
+    }
   }
 }
+
 // Function called whenever user tab on any box
 function myfunc() {
   // Setting DOM to all boxes or input field
